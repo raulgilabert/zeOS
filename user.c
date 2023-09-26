16926f0fe3f066b3962ea2dc1e3a5cca2e9b875b
Raul Gilabert <raulgilabert@proton.me>
2023-09-26 07:34:36 +0000

    first commit

---
+++ user.c
@@ -0,0 +1,23 @@
+#include <libc.h>
+
+char buff[24];
+
+int pid;
+
+int
+add(int par1, int par2)
+{
+  return par1 + par2;
+}
+
+int __attribute__ ((__section__(".text.main")))
+  main(void)
+{
+    /* Next line, tries to move value 0 to CR3 register. This register is a
+       privileged one, and so it will raise an exception */
+     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+
+  int num = addASM(0x42, 0x666);
+
+  while(1) { }
+}
